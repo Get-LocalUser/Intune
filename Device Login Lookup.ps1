d68127c6dc0c32a4a42a91f947296d08709f3cd3
Get-LocalUser <harisonkerkof@outlook.com>
2025-08-19 12:17:47 -0700

    .

---
+++ Device Login Lookup.ps1
@@ -0,0 +1,32 @@
+# Microsoft Graph Beta
+if (-not (Get-InstalledModule -Name Microsoft.Graph.Beta -ErrorAction SilentlyContinue)) {
+    Write-Host "Installing Graph module. This will take a few minutes..." -ForegroundColor Yellow
+    Install-Module -Name Microsoft.Graph.Beta -Scope CurrentUser -Force -Verbose
+}
+Import-Module Microsoft.Graph.Beta -ErrorAction Ignore
+Write-Host "Graph module imported successfully." -ForegroundColor Yellow
+
+Connect-MgGraph -Scopes "User.Read.All", "DeviceManagementManagedDevices.Read.All" -NoWelcome
+
+
+$deviceName = Read-Host "What is the device's hostname?"
+$device = Get-MgBetaDeviceManagementManagedDevice -Filter "contains(deviceName,'$deviceName')"
+
+if ($device) {
+    $lastusers = $device.UsersLoggedOn
+
+    $usersList = @()
+
+    foreach ($user in $lastusers) {
+        $lastlogon = $user.LastLogOnDateTime
+        $userobject = [PSCustomObject]@{
+            UserID = $user.userid
+            DisplayName = (Get-MgBetaUser -UserId $user.UserId).DisplayName
+            LastLoggedOnDateTime = $lastLogon
+        }
+        $usersList += $userObject
+    }
+    $usersList
+} else {
+    Write-Output "Device not found."
+}
