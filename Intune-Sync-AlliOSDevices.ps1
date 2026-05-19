e3e775b3cdecba277c1b76fb15ab349b3d053f2b
Get-LocalUser <121067700+Get-LocalUser@users.noreply.github.com>
2026-05-19 13:35:11 -0700

    Update Intune-Sync-AlliOSDevices.ps1

--- Intune-Sync-AlliOSDevices.ps1
+++ Intune-Sync-AlliOSDevices.ps1
@@ -1,15 +1,7 @@
-# Run these if you havent used MS Graph before
-#Install-Module -Name Microsoft.Graph.DeviceManagement.Actions -Force  -AllowClobber
-#Install-Module -Name Microsoft.Graph.DeviceManagement -Force -AllowClobber
- 
-# Run below this line for normal iOS check ins
-# Importing the SDK Module
-#Import-Module -Name Microsoft.Graph.DeviceManagement.Actions
- 
+# Sync iOS devices
 Import-Module -Name "Microsoft.Graph.Beta"
 Connect-MgGraph -Scope "DeviceManagementManagedDevices.PrivilegedOperations.All", "DeviceManagementManagedDevices.ReadWrite.All", "DeviceManagementManagedDevices.Read.All" -NoWelcome
  
-#### Gets All devices
 $Devices = Get-MgBetaDeviceManagementManagedDevice -Filter "contains(operatingsystem, 'iOS')" -All
 
 Foreach ($Device in $Devices)
